21791bb7e96cb202d5d018ebcf7c92806c75e5d4
Hector Garzon <hgarzon@gmail.com>
2012-02-22 23:22:45 +0100

    Card03HU2 -> Ejemplo de validacion de email usando email_re

--- aos/views/attendant_view.py
+++ aos/views/attendant_view.py
@@ -5,10 +5,13 @@ import settings
 from django.shortcuts import render_to_response
 
 from aos.models.shout_model import Shout
+from django.core.validators import email_re
 
 def create_attendant(request):
     return render_to_response('attendant.html', {})
 
+def is_valid_email(email):
+    return True if email_re.match(email) else False
 
 def create_attendant_response(request):	
     first_name = request.POST['first_name'][:20]
@@ -17,7 +20,11 @@ def create_attendant_response(request):
     city = request.POST['city'][:20]
     catering = request.POST.has_key('catering')
 
-
+    emailValido='puede'
+    if is_valid_email(email):
+        emailValido='si'
+    else:
+        emailValido='no'
 
 
 
@@ -26,6 +33,6 @@ def create_attendant_response(request):
         attendant = Attendant.create(first_name, last_name, email, city, catering)
         ok = attendant.put()
 
-        return HttpResponse('Creado Attendant ' + first_name + ' ok')
+        return HttpResponse('Creado Attendant ' + first_name + ' ok' + emailValido)
     except Exception, e:
         return HttpResponse('Creado Attendant ' + first_name + ' err')
