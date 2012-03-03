d73e15abc725b8501d4e0f81dac64cf7fd2d42b6
Alberto Gualis <agualis@gmail.com>
2012-03-03 11:12:41 +0100

    Twitter avatar added

--- aos/models/attendant_model.py
+++ aos/models/attendant_model.py
@@ -1,11 +1,15 @@
 from google.appengine.ext import db
+from google.appengine.api import urlfetch
 
 class Attendant(db.Model):
     first_name = db.StringProperty(required=True)
     last_name = db.StringProperty(required=True)
     email = db.EmailProperty(required=True)
+    twitter_id = db.StringProperty(default='')
+    twitter_avatar = db.BlobProperty()
     city = db.StringProperty(required=True)
     catering = db.BooleanProperty(required=True)
+    
     
     def __unicode__(self):
         return self.first_name + ' ' + self.last_name
@@ -20,3 +24,11 @@ class Attendant(db.Model):
     			 catering=catering)
     
     create = staticmethod(create)
+
+    def fetch_twitter_avatar(self):
+        # TODO: test with the url fetch api
+        url = 'http://api.twitter.com/1/users/profile_image/' + self.twitter_id + '.json'
+        response = urlfetch.fetch(url)
+        if response.status_code == 200:
+            self.twitter_avatar = response.content
+            self.put()
--- aos/views/attendant_view.py
+++ aos/views/attendant_view.py
@@ -6,6 +6,7 @@ from django.shortcuts import render_to_response
 
 from aos.models.shout_model import Shout
 from django.core.validators import email_re
+import logging
 
 def create_attendant(request):
     return render_to_response('attendant.html', {})
@@ -25,10 +26,6 @@ def create_attendant_response(request):
         emailValido='si'
     else:
         emailValido='no'
-
-
-
-
     try:
         attendant = Attendant.create(first_name, last_name, email, city, catering)
         ok = attendant.put()
@@ -36,3 +33,19 @@ def create_attendant_response(request):
         return HttpResponse('Creado Attendant ' + first_name + ' ok' + emailValido)
     except Exception, e:
         return HttpResponse('Creado Attendant ' + first_name + ' err')
+
+def get_avatar(request):
+    if request.method == 'GET':
+        email = request.GET.get('email', '')
+        if is_valid_email(email):
+            attendant = Attendant.get_by_key_name(email)
+            if attendant.twitter_id:
+                if not attendant.twitter_avatar:
+                    attendant.fetch_twitter_avatar()
+                img_response = HttpResponse(mimetype="image/jpeg")
+                img_response.content = attendant.twitter_avatar 
+                return img_response
+            else:
+                return HttpResponse('No twitter account')
+        else:
+            return HttpResponse('Invalid email')    
