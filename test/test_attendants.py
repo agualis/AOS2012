6db220b6f01097211d30a5e7f7be1abe340c4eee
Julio J <papajulio21@gmail.com>
2012-03-10 16:04:53 +0100

    Adding a README files and fixing some tests that were broken due to changes in the attendant

--- test/test_attendants.py
+++ test/test_attendants.py
@@ -16,34 +16,34 @@ class AttendantTestCase(unittest.TestCase, TestBedInitializer):
         self.attendant.put()
 
     def test_create_attendance_constructor(self):
-    	attendant = Attendant.create(
+        attendant = Attendant.create(
     			'Asistente2',
     			'Apellido2',
     			'asistente2@aos.com',
     			'Zaragoza2',
     			False)
-    	attendant.put()
-    	db_attendant = Attendant.get_by_key_name('asistente2@aos.com')
-    	self.assertEquals('asistente2@aos.com',db_attendant.email)
-    	self.assertEquals('Asistente2',db_attendant.first_name)
-    	self.assertEquals('Apellido2',db_attendant.last_name)
-    	self.assertEquals('Zaragoza2',db_attendant.city)
-    	self.assertEquals(False,db_attendant.catering)
+        attendant.put()
+        db_attendant = Attendant.get_by_key_name('asistente2@aos.com')
+        self.assertEquals('asistente2@aos.com',db_attendant.email)
+        self.assertEquals('Asistente2',db_attendant.first_name)
+        self.assertEquals('Apellido2',db_attendant.last_name)
+        self.assertEquals('Zaragoza2',db_attendant.city)
+        self.assertEquals(False,db_attendant.catering)
 
     def test_create_attendance_constructor_named(self):
-    	attendant = Attendant.create(email='asistente3@aos.com',
+        attendant = Attendant.create(email='asistente3@aos.com',
        				     last_name='Apellido3',
     				     first_name='Asistente3',
     				     city='Zaragoza3',
     				     catering=True)
-    	attendant.put()
-    	db_attendant = Attendant.get_by_key_name('asistente3@aos.com')
-    	self.assertEquals('asistente3@aos.com',db_attendant.email)
-    	self.assertEquals('Asistente3',db_attendant.first_name)
-    	self.assertEquals('Apellido3',db_attendant.last_name)
-    	self.assertEquals('Zaragoza3',db_attendant.city)
-    	self.assertEquals(True,db_attendant.catering)
-	
+        attendant.put()
+        db_attendant = Attendant.get_by_key_name('asistente3@aos.com')
+        self.assertEquals('asistente3@aos.com',db_attendant.email)
+        self.assertEquals('Asistente3',db_attendant.first_name)
+        self.assertEquals('Apellido3',db_attendant.last_name)
+        self.assertEquals('Zaragoza3',db_attendant.city)
+        self.assertEquals(True,db_attendant.catering)
+
     def test_create_attendance_constructor_error_mail(self):
         self.assertRaises(attendant_model.ExMailError, Attendant.create,
                     'AsistenteError',
